b482388bd9271826c880e78822243b6d4486eddb
HayatoKarasu <138600849+HayatoKarasu@users.noreply.github.com>
2023-07-11 22:56:31 +0700

    Add files via upload

---
+++ Lesson_7__part_2.py
@@ -0,0 +1,14 @@
+﻿pro = str(input("Напишите любое предложение: "))
+
+a = len(pro)
+
+if a <= 1000:
+    b = pro.split(' ')
+    c = (''.join(b))
+    d = len(c)
+    e = a - d
+
+    print(' ' * e + c)
+    
+else:
+    print("no")
